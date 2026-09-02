5d6ae60bdf18f1a44f921c332fae30cee6eed28b
Carlitosabd <carlos.abadg.est@gmail.com>
2026-09-01 19:16:44 -0500

    fix(bloque_a): agregar Scanner en Ejercicio2

--- bloque_a/Ejercicio2.java
+++ bloque_a/Ejercicio2.java
@@ -1,22 +1,18 @@
+import java.util.Scanner;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
-        int a = 15;
-        int b = 4;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingresa un número: ");
+        int numero = scanner.nextInt();
 
-        int suma = a + b;
-        int resta = a - b;
-        int multiplicacion = a * b;
-        double division = (double) a / b;
-
-        System.out.println("Suma: " + suma);
-        System.out.println("Resta: " + resta);
-        System.out.println("Multiplicación: " + multiplicacion);
-        System.out.println("División: " + division);
-
-        if (suma % 2 == 0) {
-            System.out.println("La suma es par");
+        if (numero % 2 == 0) {
+            System.out.println("El número es par");
         } else {
-            System.out.println("La suma es impar");
+            System.out.println("El número es impar");
         }
+
+        scanner.close();
     }
 }
