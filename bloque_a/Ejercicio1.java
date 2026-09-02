a5a94ff6255ce0b6ad24fabae9c752dd94b56945
Martina Ruiz <emily.ruizo.est@uets.edu.ec>
2026-09-02 16:00:59 -0500

    feat(bloque-a): resolver ejercicios de java y logica

--- bloque_a/Ejercicio1.java
+++ bloque_a/Ejercicio1.java
@@ -1,8 +1,14 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
         // TODO: Declara aquí tus variables y asígnales valores
-
+      String nombre="Martina";
+      int edad=17;
+      double altura=1.50;
+      boolean activo= true;
         // TODO: Muestra los valores por consola
-        
+     System.out.println("Nombre: "+ nombre);
+     System.out.println("Edad: "+ edad);
+     System.out.println("Altura: "+ altura);
+     System.out.println("Estado: "+ activo);
     }
 }
