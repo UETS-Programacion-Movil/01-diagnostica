1d4c3d0161d3af5e0a3b203696351089d665fd06
Carlitosabd <carlos.abadg.est@gmail.com>
2026-09-01 19:04:27 -0500

    feat(bloque_a): resolver Ejercicio2 con condicionales

--- bloque_a/Ejercicio2.java
+++ bloque_a/Ejercicio2.java
@@ -1,12 +1,22 @@
-import java.util.Scanner;
-
 public class Ejercicio2 {
     public static void main(String[] args) {
-        // TODO: Instancia la clase Scanner
+        int a = 15;
+        int b = 4;
 
-        // TODO: Lee dos números por teclado
+        int suma = a + b;
+        int resta = a - b;
+        int multiplicacion = a * b;
+        double division = (double) a / b;
 
-        // TODO: Calcula y muestra suma, resta, multiplicación y división
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
 
+        if (suma % 2 == 0) {
+            System.out.println("La suma es par");
+        } else {
+            System.out.println("La suma es impar");
+        }
     }
 }
